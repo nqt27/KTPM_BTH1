c34634b3630c7a13856cf635be494537aa64a547
nqt271 <nqt271@gmail.com>
2023-02-16 14:34:10 +0700

    Update code

--- MyApp/src/main/java/com/mycompany/myapp/MyApp.java
+++ MyApp/src/main/java/com/mycompany/myapp/MyApp.java
@@ -12,6 +12,6 @@ package com.mycompany.myapp;
 public class MyApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
     }
 }
